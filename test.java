302ce0d352aa9fce023fe1e394760e1e8599472a
uczen <mateusz.zarzecki@tm1.edu.pl>
2024-09-25 13:25:07 +0200

    another lessonsss

--- test.java
+++ test.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 
 public class test {
   public static void main(String[] args) {
-    System.out.println("hello world");
     Scanner scanner = new Scanner(System.in);
-    String name = scanner.nextLine();
-    System.out.println(name);
+    StringBuilder text = new StringBuilder();
+
+    final String info = "Enter 3 lines of text";
+    System.out.println(info);
+
+    for(int i = 0;i<3;i++) {
+        text.append(scanner.nextLine());
+      if(i!=2) text.append("\\");
+    }
+    String changed = text.toString().replace('\'', '\"');
+    System.out.println("From " + text + " To " + changed);
+
+    scanner.nextLine();
   }
 }
